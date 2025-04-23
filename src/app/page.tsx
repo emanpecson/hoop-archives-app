3fd45d116fb5d08caa2b05fe14de51f44fd5cdd5
Emanuel Pecson <emanpecson@gmail.com>
2025-04-22 20:21:44 -0700

    refactor: :recycle: source video via aws presigned-url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,81 @@
 "use client";
 
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function HomePage() {
-	// const [file, setFile] = useState<File | null>(null);
+	// programmatic access to video elem
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const [videoUrl, setVideoUrl] = useState<string | null>(null);
+
 	const handleFileChange = async (ev: React.ChangeEvent<HTMLInputElement>) => {
 		try {
 			const vid = ev.target.files?.[0];
 
 			if (vid) {
-				// setFile(vid);
+				throwInvalidVideo(vid);
 
-				const res = await fetch(`/api/upload?filename=${vid.name}`, {
-					method: "GET",
-				});
+				await uploadToBucket(vid);
+				const src = await getVideoSourceFromBucket(vid.name);
 
-				const { presignedUrl } = await res.json();
-
-				console.log("data:", presignedUrl);
-
-				console.log("uploading to s3");
-
-				// with this presigned url we could upload as follows:
-				const uploadRes = await fetch(presignedUrl, {
-					method: "PUT",
-					headers: {
-						"Content-Type": vid.type,
-					},
-					body: vid,
-				});
-
-				console.log("upload response:", await uploadRes.json());
+				setVideoUrl(src);
 			} else {
-				console.log("Invalid file");
+				throw new Error("Video not found");
 			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const throwInvalidVideo = (file: File) => {
+		const MAX_SIZE_BYTES = 5 * 1024 * 1024 * 1024; // 5 GB
+
+		if (file.size > MAX_SIZE_BYTES) {
+			throw new Error("Video file size exceeds the 5GB limit");
+		}
+	};
+
+	const s3PresignedUrlEndpointBuilder = (
+		filename: string,
+		bucketMethod: "GET" | "PUT"
+	) => {
+		return `/api/s3-presigned-url?filename=${filename}&bucketMethod=${bucketMethod}`;
+	};
+
+	const uploadToBucket = async (vid: File) => {
+		const bucketMethod = "PUT";
+
+		try {
+			const presignedUrlResponse = await fetch(
+				s3PresignedUrlEndpointBuilder(vid.name, bucketMethod)
+			);
+
+			const { presignedUrl } = await presignedUrlResponse.json();
+
+			const bucketPutResponse = await fetch(presignedUrl, {
+				method: bucketMethod,
+				headers: { "Content-Type": vid.type },
+				body: vid,
+			});
+
+			if (!bucketPutResponse.ok) {
+				throw new Error("Unable to process this video");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const getVideoSourceFromBucket = async (filename: string) => {
+		const bucketMethod = "GET";
+
+		try {
+			const presignedUrlResponse = await fetch(
+				s3PresignedUrlEndpointBuilder(filename, bucketMethod)
+			);
+
+			const { presignedUrl } = await presignedUrlResponse.json();
+
+			return presignedUrl;
 		} catch (error) {
 			console.log(error);
 		}
@@ -41,9 +83,14 @@ export default function HomePage() {
 
 	return (
 		<div className="flex justify-center place-items-center h-screen">
-			{/* <button onClick={handleFileChange}>click me</button> */}
-
-			<input type="file" accept="video/*" onChange={handleFileChange} />
+			{videoUrl ? (
+				<video ref={videoRef} src={videoUrl} width={600} controls />
+			) : (
+				<div>
+					<p>Upload video</p>
+					<input type="file" accept="video/*" onChange={handleFileChange} />
+				</div>
+			)}
 		</div>
 	);
 }
