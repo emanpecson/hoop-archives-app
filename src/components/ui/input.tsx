facf845a1c8b452d2df479d1087883375893f191
Emanuel Pecson <emanpecson@gmail.com>
2025-04-29 19:49:58 -0700

    style: :lipstick: show border on input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -17,7 +17,7 @@ function Input({ className, type, Icon, ...props }: CustomInputProps) {
 				className={cn(
 					"flex h-9 w-full min-w-0 rounded-md px-3 py-1 outline-none disabled:pointer-events-none disabled:opacity-50",
 					"focus-visible:border-neutral-950 focus-visible:ring-neutral-950/50 focus-visible:ring-[3px] placeholder:text-input-muted",
-					"text-sm bg-input-background border-input-border pl-10 font-medium",
+					"text-sm bg-input-background border border-input-border pl-10 font-medium",
 					className
 				)}
 				{...props}
