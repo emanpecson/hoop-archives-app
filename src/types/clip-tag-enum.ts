bdb999c4ddb6a9359a17f53c22a04a9b1a8f3152
Emanuel Pecson <emanpecson@gmail.com>
2025-05-04 00:27:52 -0700

    refactor: :recycle: update tags

--- src/types/clip-tag-enum.ts
+++ src/types/clip-tag-enum.ts
@@ -5,6 +5,7 @@ export enum ClipTag {
 	PUT_BACK = "#putback",
 	ALLEY_OOP = "#alleyoop",
 	CIRCUS = "#circus",
+
 	MID_RANGE = "#midrange",
 	LONG_RANGE = "#longrange",
 	DEEP = "#deep",
@@ -12,12 +13,22 @@ export enum ClipTag {
 	FADE_AWAY = "#fadeaway",
 	CATCH_AND_SHOOT = "#catchandshoot",
 	OFF_THE_DRIBBLE = "#offthedribble",
+
+	OPEN = "#open",
+	CONTESTED = "#contested",
+	SMOTHERED = "#smothered",
+	FAKE = "#fake",
 	AND1 = "#and1",
+	HUSTLE = "#hustle",
 	HIGHLIGHT = "#highlight",
 	GAME_WINNER = "#gamewinner",
+	HANDLES = "#handles",
+
 	PICK_AND_ROLL = "#pickandroll",
 	PICK_AND_POP = "#pickandpop",
+	CUT = "#cut",
 	NO_LOOK = "#nolook",
-	CONTESTED = "#contested",
-	OPEN = "#open",
+
+	BLOCK = "#block",
+	STRIP = "#strip",
 }
