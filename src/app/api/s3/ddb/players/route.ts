481df4ff63888b8ab85c9568b5eba724cee39ee1
Emanuel Pecson <emanpecson@gmail.com>
2025-05-04 23:25:55 -0700

    refactor: :recycle: use query-command for global indexing by first-name

--- src/app/api/s3/ddb/players/route.ts
+++ src/app/api/s3/ddb/players/route.ts
@@ -3,11 +3,21 @@ import {
 	ScanCommand,
 	DynamoDBDocumentClient,
 	ScanCommandInput,
+	QueryCommandInput,
+	QueryCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { NextRequest, NextResponse } from "next/server";
 
 const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 const docClient = DynamoDBDocumentClient.from(client);
+
+const getExclusiveStartKey = (keyQuery: string) => {
+	if (keyQuery !== "undefined") {
+		return JSON.parse(keyQuery);
+	} else {
+		return undefined;
+	}
+};
 
 export async function GET(req: NextRequest) {
 	const query = {
@@ -15,34 +25,40 @@ export async function GET(req: NextRequest) {
 		search: req.nextUrl.searchParams.get("search"),
 	};
 
-	const params: ScanCommandInput = {
-		TableName: process.env.AWS_DDB_PLAYERS_TABLE,
-		Limit: 4, // items per page
-		ExclusiveStartKey:
-			query.exclusiveStartKey !== "undefined"
-				? JSON.parse(query.exclusiveStartKey as string)
-				: undefined,
-	};
-
-	// filter by first/last name
-	if (query.search) {
-		params.FilterExpression =
-			"begins_with(#firstName, :q) OR begins_with(#lastName, :q)";
-		params.ExpressionAttributeNames = {
-			"#firstName": "firstName",
-			"#lastName": "lastName",
-		};
-		params.ExpressionAttributeValues = {
-			":q": query.search,
-		};
-	}
-
 	try {
-		const { Items, LastEvaluatedKey } = await docClient.send(
-			new ScanCommand(params)
-		);
+		if (query.search) {
+			const params: QueryCommandInput = {
+				TableName: process.env.AWS_DDB_PLAYERS_TABLE,
+				IndexName: "FirstNameIndex",
+				KeyConditionExpression: "#firstName = :firstName",
+				ExpressionAttributeNames: { "#firstName": "firstName" },
+				ExpressionAttributeValues: { ":firstName": query.search },
+				Limit: 4,
+				ExclusiveStartKey: getExclusiveStartKey(
+					query.exclusiveStartKey as string
+				),
+			};
 
-		return NextResponse.json({ Items, LastEvaluatedKey }, { status: 200 });
+			console.log("params:", params);
+
+			const { Items, LastEvaluatedKey } = await docClient.send(
+				new QueryCommand(params)
+			);
+			return NextResponse.json({ Items, LastEvaluatedKey }, { status: 200 });
+		} else {
+			const params: ScanCommandInput = {
+				TableName: process.env.AWS_DDB_PLAYERS_TABLE,
+				Limit: 4,
+				ExclusiveStartKey: getExclusiveStartKey(
+					query.exclusiveStartKey as string
+				),
+			};
+
+			const { Items, LastEvaluatedKey } = await docClient.send(
+				new ScanCommand(params)
+			);
+			return NextResponse.json({ Items, LastEvaluatedKey }, { status: 200 });
+		}
 	} catch (err) {
 		console.log(err);
 		return NextResponse.json({ error: "Server error" }, { status: 500 });
