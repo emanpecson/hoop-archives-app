e87acd6e9719a831815b67e5dda6a7950d5a0370
Emanuel Pecson <emanpecson@gmail.com>
2025-06-19 00:47:51 -0700

    style: :lipstick: increase rounding on card-button

--- src/components/card-button.tsx
+++ src/components/card-button.tsx
@@ -13,7 +13,7 @@ export default function CardButton(props: CardButtonProps) {
 			onClick={props.onClick}
 			disabled={props.disabled}
 			className={cn(
-				"text-left block w-full rounded-md p-3 outline-none disabled:opacity-50 text-sm bg-input-background hover:bg-input-background/70 duration-150 cursor-pointer border border-input-border place-items-center disabled:pointer-events-none inset-shadow-sm inset-shadow-input-border/60",
+				"text-left block w-full rounded-lg p-3 outline-none disabled:opacity-50 text-sm bg-input-background hover:bg-input-background/70 duration-150 cursor-pointer border border-input-border place-items-center disabled:pointer-events-none inset-shadow-sm inset-shadow-input-border/60",
 				props.className
 			)}
 		>
