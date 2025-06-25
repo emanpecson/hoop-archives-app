87037e6a9cfd87889ebcac1452fc0d74663b0848
Emanuel Pecson <emanpecson@gmail.com>
2025-06-24 17:17:29 -0700

    fix: :bug: must await params on games request

--- src/app/api/ddb/games/[leagueId]/[title]/route.ts
+++ src/app/api/ddb/games/[leagueId]/[title]/route.ts
@@ -6,11 +6,10 @@ const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 const docClient = DynamoDBDocumentClient.from(client);
 
 export async function GET(
-	req: NextRequest,
-	{ params }: { params: { leagueId: string; title: string } }
+	_req: NextRequest,
+	{ params }: { params: Promise<{ leagueId: string; title: string }> }
 ) {
-	console.log("params:", params);
-	const { leagueId, title } = params;
+	const { leagueId, title } = await params;
 
 	try {
 		const command = new GetCommand({
