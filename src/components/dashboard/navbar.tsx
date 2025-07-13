e562a31e6ad2dbb51e055153ea14ed7f5696a3aa
Emanuel Pecson <emanpecson@gmail.com>
2025-07-13 02:24:19 -0700

    style: :lipstick: show active route

--- src/components/dashboard/navbar.tsx
+++ src/components/dashboard/navbar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { PageRouter } from "@/data/routes";
 import DashboardCard from "./dashboard-card";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 interface NarbarProps {
 	leagueId: string;
@@ -8,6 +12,7 @@ interface NarbarProps {
 
 export default function Navbar(props: NarbarProps) {
 	const pageRouter = new PageRouter(props.leagueId);
+	const pathname = usePathname();
 
 	return (
 		<DashboardCard className="h-20 flex place-items-center px-8 space-x-8">
@@ -18,16 +23,24 @@ export default function Navbar(props: NarbarProps) {
 			</span>
 
 			<nav className="flex space-x-2 font-medium text-base">
-				{pageRouter.getAllRoutes().map((route, i) => (
-					<Link
-						href={route.path}
-						key={i}
-						className="flex place-items-center space-x-2 px-3 py-1.5 hover:bg-neutral-800/60 text-neutral-500 hover:text-white rounded-xl duration-100"
-					>
-						<route.Icon size={20} />
-						<span>{route.name}</span>
-					</Link>
-				))}
+				{pageRouter.getAllRoutes().map((route, i) => {
+					const active = pathname === route.path;
+					return (
+						<Link
+							href={route.path}
+							key={i}
+							className={cn(
+								"flex place-items-center space-x-2 px-3 py-1.5 rounded-xl duration-100",
+								active
+									? "text-white bg-neutral-800/60"
+									: "hover:bg-neutral-800/60 text-neutral-500 hover:text-white"
+							)}
+						>
+							<route.Icon size={20} />
+							<span>{route.name}</span>
+						</Link>
+					);
+				})}
 			</nav>
 		</DashboardCard>
 	);
