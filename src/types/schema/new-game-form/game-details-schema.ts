6de27d9ec536c54cbd544c8d1faf6d9d5ff08632
Emanuel Pecson <emanpecson@gmail.com>
2025-08-05 22:30:50 -0700

    fix: :fire: remove unnecessary zod check on gmae-details-schema

--- src/types/schema/new-game-form/game-details-schema.ts
+++ src/types/schema/new-game-form/game-details-schema.ts
@@ -1,14 +1,9 @@
 import { z } from "zod";
 
-const fileNameSafeRegex = /^[a-zA-Z0-9_-]+$/;
-
 export const gameDetailsSchema = z.object({
 	title: z.string().min(1, "Required").max(30, "Max length of 30 characters"),
 	date: z.date({ message: "Required" }),
-	type: z.string().min(1, "Required").regex(fileNameSafeRegex, {
-		message:
-			"Must only contain letters, numbers, underscores, or dashes (no spaces or dots)",
-	}),
+	type: z.string().min(1, "Required"),
 });
 
 export type GameDetailsFormFields = z.infer<typeof gameDetailsSchema>;
