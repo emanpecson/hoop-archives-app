e8eb0a22fd2fec1e959b0a2d1c0394243a38077d
Emanuel Pecson <emanpecson@gmail.com>
2025-09-06 19:46:11 -0700

    style: :lipstick: confirm-dialog tab reformatting

--- src/components/confirm-dialog.tsx
+++ src/components/confirm-dialog.tsx
@@ -1,77 +1,77 @@
 import { useState } from "react";
 import {
-	Dialog,
-	DialogClose,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { Loader2Icon } from "lucide-react";
 
 interface ConfirmDialogProps {
-	onConfirm: () => void;
-	children: React.ReactNode;
-	confirmPrompt?: string;
-	title?: string;
-	description?: string;
-	loading?: boolean;
+  onConfirm: () => void;
+  children: React.ReactNode;
+  confirmPrompt?: string;
+  title?: string;
+  description?: string;
+  loading?: boolean;
 }
 
 export default function ConfirmDialog(props: ConfirmDialogProps) {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	const handleClose = (flag: boolean) => {
-		if (!props.loading) {
-			setOpen(flag);
-		}
-	};
+  const handleClose = (flag: boolean) => {
+    if (!props.loading) {
+      setOpen(flag);
+    }
+  };
 
-	return (
-		<Dialog open={open} onOpenChange={handleClose}>
-			<DialogTrigger asChild>{props.children}</DialogTrigger>
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogTrigger asChild>{props.children}</DialogTrigger>
 
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>{props.title || "Confirm"}</DialogTitle>
-					<DialogDescription>
-						{props.description || "Are you sure you want to continue?"}
-					</DialogDescription>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{props.title || "Confirm"}</DialogTitle>
+          <DialogDescription>
+            {props.description || "Are you sure you want to continue?"}
+          </DialogDescription>
 
-					<DialogFooter>
-						<div className="w-full flex justify-end space-x-2">
-							<DialogClose asChild>
-								<Button
-									type="button"
-									variant="input"
-									size="sm"
-									className="w-fit"
-									disabled={props.loading}
-								>
-									Cancel
-								</Button>
-							</DialogClose>
+          <DialogFooter>
+            <div className="w-full flex justify-end space-x-2">
+              <DialogClose asChild>
+                <Button
+                  type="button"
+                  variant="input"
+                  size="sm"
+                  className="w-fit"
+                  disabled={props.loading}
+                >
+                  Cancel
+                </Button>
+              </DialogClose>
 
-							<Button
-								type="button"
-								variant="input"
-								size="sm"
-								className="w-fit"
-								onClick={props.onConfirm}
-								disabled={props.loading}
-							>
-								{props.loading && (
-									<Loader2Icon className="animate-spin" strokeWidth={1.5} />
-								)}
-								<span>{props.confirmPrompt ?? "Confirm"}</span>
-							</Button>
-						</div>
-					</DialogFooter>
-				</DialogHeader>
-			</DialogContent>
-		</Dialog>
-	);
+              <Button
+                type="button"
+                variant="input"
+                size="sm"
+                className="w-fit"
+                onClick={props.onConfirm}
+                disabled={props.loading}
+              >
+                {props.loading && (
+                  <Loader2Icon className="animate-spin" strokeWidth={1.5} />
+                )}
+                <span>{props.confirmPrompt ?? "Confirm"}</span>
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
