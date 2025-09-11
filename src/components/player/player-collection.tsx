3749a60a667d409efe4cc8b2c6309de4316fabab
Emanuel Pecson <emanpecson@gmail.com>
2025-09-10 19:58:13 -0700

    refactor :recycle: include players in public route + hide new-player button

--- src/components/player/player-collection.tsx
+++ src/components/player/player-collection.tsx
@@ -9,55 +9,62 @@ import LoadingPrompt from "../loading-prompt";
 import EmptyPrompt from "../empty-prompt";
 import { Button } from "../ui/button";
 import NewPlayerDialog from "../new-player/new-player-dialog";
+import { useSession } from "next-auth/react";
+import useRoleCheck from "@/hooks/use-role-check";
 
 interface PlayerCollectionProps {
-	leagueId: string;
+  leagueId: string;
 }
 
 export default function PlayerCollection(props: PlayerCollectionProps) {
-	const [players, setPlayers] = useState<Player[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [newPlayerDialogOpen, setNewPlayerDialogOpen] = useState(false);
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [newPlayerDialogOpen, setNewPlayerDialogOpen] = useState(false);
 
-	useLoadData({
-		endpoint: `/api/ddb/${props.leagueId}/players`,
-		onDataLoaded: setPlayers,
-		setIsLoading,
-		onError: () => toast.error("Error fetching players"),
-	});
+  const { data: session } = useSession();
+  const { isBetaUser } = useRoleCheck(session);
 
-	return (
-		<>
-			<div className="w-full h-full flex-col space-y-4">
-				<div className="flex justify-end">
-					<Button
-						variant="input"
-						className="w-fit"
-						onClick={() => setNewPlayerDialogOpen(true)}
-					>
-						New player
-					</Button>
-				</div>
+  useLoadData({
+    endpoint: `/api/ddb/${props.leagueId}/players`,
+    onDataLoaded: setPlayers,
+    setIsLoading,
+    onError: () => toast.error("Error fetching players"),
+  });
 
-				<div>
-					{isLoading ? (
-						<LoadingPrompt text="Loading players. Please wait..." />
-					) : players.length > 0 ? (
-						<div className="flex flex-row flex-wrap gap-8 justify-center">
-							{players.map((player, i) => (
-								<PlayerCard player={player} key={i} />
-							))}
-						</div>
-					) : (
-						<EmptyPrompt text="No players" goBackUrl="/" />
-					)}
-				</div>
-			</div>
+  return (
+    <>
+      <div className="w-full h-full flex-col space-y-4">
+        <div className="flex justify-end">
+          {isBetaUser() && (
+            <Button
+              variant="input"
+              className="w-fit"
+              onClick={() => setNewPlayerDialogOpen(true)}
+            >
+              New player
+            </Button>
+          )}
+        </div>
 
-			<NewPlayerDialog
-				open={newPlayerDialogOpen}
-				setOpen={setNewPlayerDialogOpen}
-			/>
-		</>
-	);
+        <div>
+          {isLoading ? (
+            <LoadingPrompt text="Loading players. Please wait..." />
+          ) : players.length > 0 ? (
+            <div className="flex flex-row flex-wrap gap-8 justify-center">
+              {players.map((player, i) => (
+                <PlayerCard player={player} key={i} />
+              ))}
+            </div>
+          ) : (
+            <EmptyPrompt text="No players" goBackUrl="/" />
+          )}
+        </div>
+      </div>
+
+      <NewPlayerDialog
+        open={newPlayerDialogOpen}
+        setOpen={setNewPlayerDialogOpen}
+      />
+    </>
+  );
 }
--- src/lib/page-router.tsx
+++ src/lib/page-router.tsx
@@ -1,98 +1,98 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { PageRoute } from "@/types/page-route";
 import {
-	FilmIcon,
-	HouseIcon,
-	PaperclipIcon,
-	SquareScissorsIcon,
-	UsersIcon,
+  FilmIcon,
+  HouseIcon,
+  PaperclipIcon,
+  SquareScissorsIcon,
+  UsersIcon,
 } from "lucide-react";
 import { Session, User } from "next-auth";
 
 // * using this method so that I can access a specific page-route by name
-const publicPageKeys = ["home", "highlightReel"] as const;
+const publicPageKeys = ["home", "highlightReel", "players"] as const;
 type PublicPageKey = (typeof publicPageKeys)[number];
 
-const protectedPageKeys = ["highlightBuilder", "draft", "players"] as const;
+const protectedPageKeys = ["highlightBuilder", "draft"] as const;
 type ProtectedPageKey = (typeof protectedPageKeys)[number];
 
 export class PageRouter {
-	private static betaRoleName: string = "BetaUser";
+  private static betaRoleName: string = "BetaUser";
 
-	private publicRoutes: Record<PublicPageKey, PageRoute>;
-	private protectedRoutes: Record<ProtectedPageKey, PageRoute>;
-	private isBetaUser: boolean = true;
+  private publicRoutes: Record<PublicPageKey, PageRoute>;
+  private protectedRoutes: Record<ProtectedPageKey, PageRoute>;
+  private isBetaUser: boolean = true;
 
-	// * ------------------------------------------------ *
+  // * ------------------------------------------------ *
 
-	constructor(leagueId: string, session: Session | null) {
-		if (session && session.user)
-			this.isBetaUser = this.checkIsBetaUser(session.user);
+  constructor(leagueId: string, session: Session | null) {
+    if (session && session.user)
+      this.isBetaUser = this.checkIsBetaUser(session.user);
 
-		this.publicRoutes = {
-			home: { name: "Home", path: `/league/${leagueId}`, Icon: HouseIcon },
-			highlightReel: {
-				name: "Highlight Reel",
-				path: `/league/${leagueId}/highlight-reel`,
-				Icon: FilmIcon,
-			},
-		};
+    this.publicRoutes = {
+      home: { name: "Home", path: `/league/${leagueId}`, Icon: HouseIcon },
+      highlightReel: {
+        name: "Highlight Reel",
+        path: `/league/${leagueId}/highlight-reel`,
+        Icon: FilmIcon,
+      },
+      players: {
+        name: "Players",
+        path: `/league/${leagueId}/player`,
+        Icon: UsersIcon,
+      },
+    };
 
-		this.protectedRoutes = {
-			highlightBuilder: {
-				name: "Highlight Builder",
-				path: `/league/${leagueId}/highlight-builder`,
-				Icon: PaperclipIcon,
-			},
-			draft: {
-				name: "Draft",
-				path: `/league/${leagueId}/draft`,
-				Icon: SquareScissorsIcon,
-			},
-			players: {
-				name: "Players",
-				path: `/league/${leagueId}/player`,
-				Icon: UsersIcon,
-			},
-		};
-	}
+    this.protectedRoutes = {
+      highlightBuilder: {
+        name: "Highlight Builder",
+        path: `/league/${leagueId}/highlight-builder`,
+        Icon: PaperclipIcon,
+      },
+      draft: {
+        name: "Draft",
+        path: `/league/${leagueId}/draft`,
+        Icon: SquareScissorsIcon,
+      },
+    };
+  }
 
-	// * ------------------------------------------------ *
+  // * ------------------------------------------------ *
 
-	public getAccessibleRoutes = (): PageRoute[] => {
-		if (this.isBetaUser) return Object.values(this.publicRoutes);
+  public getAccessibleRoutes = (): PageRoute[] => {
+    if (this.isBetaUser) return Object.values(this.publicRoutes);
 
-		return Object.values(this.publicRoutes).concat(
-			Object.values(this.protectedRoutes)
-		);
-	};
+    return Object.values(this.publicRoutes).concat(
+      Object.values(this.protectedRoutes)
+    );
+  };
 
-	public getRoute = (key: PublicPageKey | ProtectedPageKey): PageRoute => {
-		if (key in this.publicRoutes) {
-			return this.publicRoutes[key as PublicPageKey];
-		}
-		return this.protectedRoutes[key as ProtectedPageKey];
-	};
+  public getRoute = (key: PublicPageKey | ProtectedPageKey): PageRoute => {
+    if (key in this.publicRoutes) {
+      return this.publicRoutes[key as PublicPageKey];
+    }
+    return this.protectedRoutes[key as ProtectedPageKey];
+  };
 
-	public isViolatingRouteAccess = (pathname: string): boolean => {
-		return this.containsProtectedRoutes(pathname) && this.isBetaUser;
-	};
+  public isViolatingRouteAccess = (pathname: string): boolean => {
+    return this.containsProtectedRoutes(pathname) && this.isBetaUser;
+  };
 
-	// * ------------------------------------------------ *
+  // * ------------------------------------------------ *
 
-	private checkIsBetaUser = (user: User) => {
-		const { groups } = user;
+  private checkIsBetaUser = (user: User) => {
+    const { groups } = user;
 
-		// defualt role is beta user
-		if (!groups || groups.length === 0) return true;
+    // defualt role is beta user
+    if (!groups || groups.length === 0) return true;
 
-		// otherwise, check if user belongs in beta group
-		return groups.some((x) => x === PageRouter.betaRoleName);
-	};
+    // otherwise, check if user belongs in beta group
+    return groups.some((x) => x === PageRouter.betaRoleName);
+  };
 
-	private containsProtectedRoutes = (pathname: string): boolean => {
-		return Object.values(this.protectedRoutes).some((route) =>
-			pathname.startsWith(route.path)
-		);
-	};
+  private containsProtectedRoutes = (pathname: string): boolean => {
+    return Object.values(this.protectedRoutes).some((route) =>
+      pathname.startsWith(route.path)
+    );
+  };
 }
