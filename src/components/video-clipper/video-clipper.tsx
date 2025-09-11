60cc8472c9a47bce167b97523336330004fa9528
Emanuel Pecson <emanpecson@gmail.com>
2025-09-10 19:29:39 -0700

    refactor: :recycle: pause vid on clip creation

--- src/components/video-clipper/video-clipper.tsx
+++ src/components/video-clipper/video-clipper.tsx
@@ -11,65 +11,69 @@ import { ClipDraft } from "@/types/clip-draft";
 import { useVideoClipperStore } from "@/hooks/use-video-clipper-store";
 
 interface VideoClipperProps {
-	draftId: string;
+  draftId: string;
 }
 
 export default function VideoClipper(props: VideoClipperProps) {
-	const [newClipDialogOpen, setNewClipDialogOpen] = useState(false);
-	const { draft, setDraft, fetchDraft, fetchSource, sortClips } =
-		useVideoClipperStore((state) => ({
-			draft: state.draft,
-			setDraft: state.setDraft,
-			fetchDraft: state.fetchDraft,
-			fetchSource: state.fetchSource,
-			sortClips: state.sortClips,
-		}));
+  const [newClipDialogOpen, setNewClipDialogOpen] = useState(false);
+  const { draft, setDraft, fetchDraft, fetchSource, sortClips, videoRef } =
+    useVideoClipperStore((state) => ({
+      draft: state.draft,
+      setDraft: state.setDraft,
+      fetchDraft: state.fetchDraft,
+      fetchSource: state.fetchSource,
+      sortClips: state.sortClips,
+      videoRef: state.videoRef,
+    }));
 
-	// for defining further clip details
-	const [newClipTime, setNewClipTime] = useState<ClipTime | null>(null);
+  // for defining further clip details
+  const [newClipTime, setNewClipTime] = useState<ClipTime | null>(null);
 
-	const handleClipTime = (clipTime: ClipTime) => {
-		setNewClipTime(clipTime);
-		setNewClipDialogOpen(true);
-	};
+  const handleClipTime = (clipTime: ClipTime) => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+    setNewClipTime(clipTime);
+    setNewClipDialogOpen(true);
+  };
 
-	const handleClipCreate = (newClip: ClipDraft) => {
-		const sortedClips = sortClips([...draft!.clipDrafts, newClip]);
-		setDraft({ ...draft!, clipDrafts: sortedClips });
-		setNewClipTime(null);
-	};
+  const handleClipCreate = (newClip: ClipDraft) => {
+    const sortedClips = sortClips([...draft!.clipDrafts, newClip]);
+    setDraft({ ...draft!, clipDrafts: sortedClips });
+    setNewClipTime(null);
+  };
 
-	useEffect(() => {
-		if (props.draftId) fetchDraft(props.draftId);
-	}, [props.draftId, fetchDraft]);
+  useEffect(() => {
+    if (props.draftId) fetchDraft(props.draftId);
+  }, [props.draftId, fetchDraft]);
 
-	useEffect(() => {
-		if (draft) fetchSource(draft.bucketKey);
-	}, [draft, fetchSource]);
+  useEffect(() => {
+    if (draft) fetchSource(draft.bucketKey);
+  }, [draft, fetchSource]);
 
-	return (
-		<>
-			<div className="flex w-full h-full gap-dashboard">
-				<div className="flex flex-col w-full h-full gap-dashboard min-w-0">
-					<div className="flex w-full gap-dashboard h-full min-h-0">
-						<DraftPlayer />
-					</div>
-					<div className="h-fit flex flex-col gap-dashboard">
-						{draft && <VideoController onClipTime={handleClipTime} />}
-						<ClipController />
-					</div>
-				</div>
-				<GameDetails />
-			</div>
+  return (
+    <>
+      <div className="flex w-full h-full gap-dashboard">
+        <div className="flex flex-col w-full h-full gap-dashboard min-w-0">
+          <div className="flex w-full gap-dashboard h-full min-h-0">
+            <DraftPlayer />
+          </div>
+          <div className="h-fit flex flex-col gap-dashboard">
+            {draft && <VideoController onClipTime={handleClipTime} />}
+            <ClipController />
+          </div>
+        </div>
+        <GameDetails />
+      </div>
 
-			{newClipTime && draft && (
-				<NewClipDialog
-					open={newClipDialogOpen}
-					clipTime={newClipTime}
-					onClipCreate={handleClipCreate}
-					onClose={setNewClipDialogOpen}
-				/>
-			)}
-		</>
-	);
+      {newClipTime && draft && (
+        <NewClipDialog
+          open={newClipDialogOpen}
+          clipTime={newClipTime}
+          onClipCreate={handleClipCreate}
+          onClose={setNewClipDialogOpen}
+        />
+      )}
+    </>
+  );
 }
