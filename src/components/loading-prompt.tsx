048c9a8a546d61b90b7b27576f66eee52daeacff
Emanuel Pecson <emanpecson@gmail.com>
2025-11-26 19:00:48 -0800

    style: :lipstick: apply padding to loadint-prompt

--- src/components/loading-prompt.tsx
+++ src/components/loading-prompt.tsx
@@ -3,23 +3,23 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 
 interface LoadingPromptProps {
-	text: string;
-	goBackUrl?: string;
+  text: string;
+  goBackUrl?: string;
 }
 
 export default function LoadingPrompt(props: LoadingPromptProps) {
-	return (
-		<div className="w-full h-1/2 flex flex-col justify-center place-items-center text-neutral-400">
-			<p className="animate-bounce text-2xl">🏀</p>
-			<div className="flex space-x-2">
-				<Loader2Icon className="animate-spin" />
-				<p className="font-medium text-base">{props.text}</p>
-			</div>
-			{props.goBackUrl && (
-				<Button variant="input" className="w-fit mt-3">
-					<Link href={props.goBackUrl}>Go back</Link>
-				</Button>
-			)}
-		</div>
-	);
+  return (
+    <div className="p-8 w-full h-1/2 flex flex-col justify-center place-items-center text-neutral-400">
+      <p className="animate-bounce text-2xl">🏀</p>
+      <div className="flex space-x-2">
+        <Loader2Icon className="animate-spin" />
+        <p className="font-medium text-base">{props.text}</p>
+      </div>
+      {props.goBackUrl && (
+        <Button variant="input" className="w-fit mt-3">
+          <Link href={props.goBackUrl}>Go back</Link>
+        </Button>
+      )}
+    </div>
+  );
 }
